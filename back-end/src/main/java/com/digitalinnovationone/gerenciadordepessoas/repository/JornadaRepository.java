83cb713b0653eb88894e969c2daccc17a20047de
João Paulo Dias Araújo <dias.araujo.joao@gmail.com>
2021-09-08 21:18:52 -0300

    Second commit

--- back-end/src/main/java/com/digitalinnovationone/gerenciadordepessoas/repository/JornadaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.digitalinnovationone.gerenciadordepessoas.repository;
-
-import com.digitalinnovationone.gerenciadordepessoas.model.JornadaTrabalho;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JornadaRepository extends JpaRepository<JornadaTrabalho, Long> {
-}
